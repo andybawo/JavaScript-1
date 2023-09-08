33229d7a0d239fff1c2aaa72597a9672f9ee3554
andybawo <bawoanderson@gmail.com>
2023-09-08 13:27:47 +0100

    modified todo-list

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,35 @@
+const todoList = ['make dinner', 'wash dishes'];
+
+renderTodoList();
+
+function renderTodoList () {
+let todoListHTML = '';
+
+for (let i = 0; i < todoList.length; i++) {
+  const todo = todoList[i];
+  const html = `<p>${todo}</p>`;    // the code in this function tells us how to put the array on the webpage
+  todoListHTML += html;
+}
+console.log(todoListHTML);
+
+document.querySelector('.js-todo-list')
+  .innerHTML = todoListHTML
+}
+
+function addOnKeyDown(event) {
+  if (event.key === 'Enter') {
+    addTodo();
+  }
+}
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value
+  
+  todoList.push(name);
+  console.log(todoList);
+
+  inputElement.value = '';
+
+  renderTodoList();
+}
