c9c67207f5abb783bab38b3faf6f9c8ad9fe9675
andybawo <bawoanderson@gmail.com>
2023-09-13 17:26:23 +0100

    started arrays part 2

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -5,9 +5,16 @@ renderTodoList();
 function renderTodoList () {
 let todoListHTML = '';
 
-for (let i = 0; i < todoList.length; i++) {
-  const todo = todoList[i];
-  const html = `<p>${todo}</p>`;    // the code in this function tells us how to put the array on the webpage
+for (let i = 0; i < todoList.length; i++) {  //
+  const todo = todoList[i];                 //  GENERATING THE HTML
+  const html = `
+    <p>
+      ${todo} 
+      <button onclick="
+        todoList.splice(${i}, 1);  
+        renderTodoList();
+      ">Delete</button>
+    </p>`;    // the code in this function tells us how to put the array on the webpage
   todoListHTML += html;
 }
 console.log(todoListHTML);
@@ -15,6 +22,8 @@ console.log(todoListHTML);
 document.querySelector('.js-todo-list')
   .innerHTML = todoListHTML
 }
+
+
 
 function addOnKeyDown(event) {
   if (event.key === 'Enter') {
