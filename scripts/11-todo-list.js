8b1e44d3bca5b39588b350a2f596e0a0b0cb72fd
andybawo <bawoanderson@gmail.com>
2023-09-13 18:55:19 +0100

    finished todo-list

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,10 @@
-const todoList = ['make dinner', 'wash dishes'];
+const todoList = [{
+  name:'make dinner', 
+  dueDate: '2023-09-13'
+}, {
+  name: 'wash dishes',
+  dueDate: '2023-09-13'
+  }];
 
 renderTodoList();
 
@@ -6,15 +12,18 @@ function renderTodoList () {
 let todoListHTML = '';
 
 for (let i = 0; i < todoList.length; i++) {  //
-  const todo = todoList[i];                 //  GENERATING THE HTML
+  const todoObject = todoList[i];                 //  GENERATING THE HTML
+  //const name = todoObject.name;
+  //const dueDate = todoObject.dueDate;
+  const { name, dueDate } = todoObject; //shortcut for line 16 and 17
   const html = `
-    <p>
-      ${todo} 
-      <button onclick="
-        todoList.splice(${i}, 1);  
-        renderTodoList();
-      ">Delete</button>
-    </p>`;    // the code in this function tells us how to put the array on the webpage
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+      todoList.splice(${i}, 1);  
+      renderTodoList();
+    " class="delete-todo-btn">Delete</button>
+    `;    // the code in this function tells us how to put the array on the webpage
   todoListHTML += html;
 }
 console.log(todoListHTML);
@@ -33,11 +42,17 @@ function addOnKeyDown(event) {
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
-  const name = inputElement.value
-  
-  todoList.push(name);
-  console.log(todoList);
+  const name = inputElement.value //the code that gets the name inputed
 
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value //code that gets the date selected
+  
+  todoList.push({
+    //name: name,
+    //dueDate: dueDate, //if property and variable name are the same just type it out once (line 51&52)
+    name,
+    dueDate
+  });
   inputElement.value = '';
 
   renderTodoList();
